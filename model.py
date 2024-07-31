dde17816f465b42f42cf75480ee00f4f22a0aa89
George Pentchev <georgiepentch@gmail.com>
2024-07-31 14:16:49 -0400

    i need to learn pytorch

---
+++ model.py
@@ -0,0 +1,8 @@
+from pgdv import PGDV
+
+test = PGDV.from_csv("training_data/test.pgdv")
+
+
+print(test.data)
+
+
--- pgdv.py
+++ pgdv.py
@@ -1,33 +1,30 @@
+from csv import reader
+
+
 class PGDV:
     """
-    PGDV is a series of NOTES (quadruples) where
-        - the 1st element is the Pitch      (integer from 0 to 127 inclusive)
-        - the 2nd element is the Gap        (integer equal to or greater than 0)
-        - the 3rd element is the Duration   (integer equal to or greater than 0)
-        - the 4th element is the Velocity   (integer from 0 to 127 inclusive)
-    """
+    a PGDV is a series of quadruples where, in each quadruple, the elements are
 
+    1. Pitch      (integer from 0 to 127 inclusive)
+    2. Gap        (integer equal to or greater than 0)
+    3. Duration   (integer equal to or greater than 0)
+    4. Velocity   (integer from 0 to 127 inclusive)
+    """
     tps = 960  # ticks per second
 
-    def __init__(self, array):
-        self.array = array  # List of 4-tuples
+    def __init__(self, data):
+        self.data = data  # 2D array of width 4
 
     def __str__(self):
-        return "\n".join(["\t".join(i) for i in self.data])
+        return "\n".join([f"   Pitch: {i[0]}\n     Gap: {i[1]}\nDuration: {i[2]}\nVelocity: {i[3]}\n"
+                          for i in self.data])
 
     @classmethod
-    def from_txt(cls, path):
-        """
-        Creates a PGDV object from a file. NOTES are separated by newlines and NOTE elements are seperated by tabs.
-        :param path: The path and name of the file
-        :return: PGDV instance
-        """
-        f = open(path, 'r')
-        dat = [i.split(" ") for i in f.read().split("\t")]
-        f.close()
-        return cls(dat)
+    def from_csv(cls, path):
+        with open(path) as f:
+            data = list(reader(f))
+        return cls(data)
 
     @classmethod
-    def from_midi(cls, path):
+    def to_csv(cls, path):
         pass
-
