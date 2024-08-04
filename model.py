12677a82bcf15d6b908f097db5d610fa34b23487
George Pentchev <georgiepentch@gmail.com>
2024-08-04 00:34:42 -0400

    bleh

--- model.py
+++ model.py
@@ -3,28 +3,28 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 import os
-from format import PGDV
+from pgdv import PGDV
 
 
 class MusicDataset(Dataset):
     def __init__(self):
         self.dir = "./training_data"
-        self.filenames = [i for i in os.listdir(self.dir) if i[-5:] == ".pgdv"]
+        self.files = [PGDV.from_csv(os.path.join(self.dir, i)).data for i in os.listdir(self.dir) if i[-5:] == ".pgdv"]
 
     def __len__(self):
-        return len(self.filenames)
+        return len(self.files)
 
     def __getitem__(self, idx):
-        return PGDV.data_from(os.path.join(self.dir, self.filenames[idx]))
+        return self.files[idx]
 
 
 class Embedding(nn.Module):
     def __init__(self, d_p, d_gd, d_v, context):
         super().__init__()
-        self.p_emb = nn.Embedding(128, d_p)
+        self.p_emb = nn.Embedding(88, d_p)
         self.gd_emb = nn.Linear(2, d_gd)
         self.v_emb = nn.Linear(1, d_v)
-        self.positional_emb = nn.Embedding(context, d_p + d_gd, d_v)
+        self.pe = nn.Embedding(context, d_p + d_gd, d_v)
 
     def forward(self, x):  # x is a tensor of shape (batch_size, sequence_length, 4)
         pass
---
+++ pgdv.py
@@ -0,0 +1,33 @@
+from csv import reader
+
+
+class PGDV:
+    def __init__(self, data):
+        self.data = data  # 2D array of width 4
+
+    def __str__(self):
+        return "\n".join([f"   Pitch: {i[0]}\n     Gap: {i[1]}\nDuration: {i[2]}\nVelocity: {i[3]}\n"
+                          for i in self.data])
+
+    tps = 960  # ticks per second
+
+    @classmethod
+    def from_csv(cls, path):
+        with open(path) as f:
+            data = list(reader(f))
+        return cls(data)
+
+    def to_csv(self, path):
+        pass
+
+    @classmethod
+    def from_midi(cls, path):
+        pass
+
+    def to_midi(self, path):
+        pass
+
+    def transpose(self):
+        pass
+
+
